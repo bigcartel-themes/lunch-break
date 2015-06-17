41df3eb6f36fe6942317b222d810595c6475fdce
Nick Endle <nick@deadformat.net>
2015-06-17 10:19:04 -0500

    - more work on updating cart page - improved product status display for thumbnails - new default header - added magnific popup - remove from cart now works - improvements to mobile menu - added header position setting - fixed product option dropdowns - fixed max. products per row setting

---
+++ source/javascripts/store.js
@@ -0,0 +1,18 @@
+$(function() { 
+  $('.open_menu').click(function(e) {
+    e.preventDefault();
+    $('aside').toggleClass('open');
+    $('.content').toggleClass('hidden');
+  });
+  $('.remove a').click(function(e) {
+    e.preventDefault();
+    $(this).closest('li').find('input[id$=_qty]').val(0).closest('form').submit();
+  });
+  $('.product_images a').magnificPopup({
+    type:'image',
+    gallery: {
+      enabled: true
+    }
+  });
+
+})
