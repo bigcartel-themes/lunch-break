a088861c6e673572601ea2094ab30d3b5b444b58
Nick Endle <nick@deadformat.net>
2016-02-04 09:15:57 -0600

    - add error messages to individual pages - fix up cart discount/shipping area and buttons - improve cart line item display - overhaul contact form - center ‘no products found’ message on mobile - improve search form functionality - make the product image popup consistent with rest of theme styles - add new social media share buttons - change some theme defaults - give everything in general a little more breathing room and consistent spacing

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -1,20 +1,34 @@
 $(function() {
-  $('.open_menu').click(function(e) {
-    e.preventDefault();
-    $(this).toggleClass('open');
-    $('aside').toggleClass('open');
-    //$('.content').toggleClass('hidden');
-  });
-  $('.remove a').click(function(e) {
-    e.preventDefault();
-    $(this).closest('li').find('input[id$=_qty]').val(0).closest('form').submit();
-  });
   $('.product_images a').magnificPopup({
     type:'image',
     gallery: {
       enabled: true
     }
   });
+  $('.open_menu').click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('open');
+    $('aside').toggleClass('open');
+  });
+  $('.remove a').click(function(e) {
+    e.preventDefault();
+    $(this).closest('li').find('input[id$=_qty]').val(0).closest('form').submit();
+  });
+  
+  $('[name*="cart[update]"], [name="cart[discount_code]"], [name="cart[shipping_country_id]"]').on('change',function() {
+    $(this).closest('form').submit();
+  });
+  
+  $('.search_form input')
+    .on('focus', function() { 
+      $(this).parent().addClass('focus');
+    })
+    .on('blur',function() {
+      $(this).parent().removeClass('focus');
+      $(this).val('');
+  })
+    
+
   $(window).resize(function() { 
     $('body').css('margin-bottom', $('footer').height());
   });
