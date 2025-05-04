e1d0d045af586e2d98ac64d286c47edfdf4f1b61
Trevin Chow <trevin@trevinchow.com>
2025-05-03 18:48:22 -0700

    fix: remove reference to `currencyLocale`

--- source/javascripts/functions.js
+++ source/javascripts/functions.js
@@ -104,7 +104,7 @@ const htmlHighlight = function(element, newText) {
  */
 function formatMoney(amount, withDelimiter = true, withSign = true, withCode = false) {
   const currency = window.bigcartel?.account?.currency || 'USD';
-  const locale = window.bigcartel?.account?.currencyLocale || 'en-US';
+  const locale = navigator.language || 'en-US';
   const moneyFormat = window.bigcartel?.account?.moneyFormat || 'sign';
   
   switch (moneyFormat) {
